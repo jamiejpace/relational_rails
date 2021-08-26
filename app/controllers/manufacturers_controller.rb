b1b95772904900a345c0712a70fd03f0eb7d845d
Jessica Jones <daddehjones@gmail.com>
2021-08-26 18:18:16 -0400

    Create show method.

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -2,4 +2,8 @@ class ManufacturersController < ApplicationController
   def index
     @manufacturers = Manufacturer.all
   end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
 end
