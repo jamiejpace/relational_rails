242c95f241fc2a0162d96be3a25d4db510aa3ac3
Jessica Jones <daddehjones@gmail.com>
2021-08-27 17:36:04 -0400

    Add boozes index and show route.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/brews/:id', to: 'brews#show'
   get '/manufacturers', to: 'manufacturers#index'
   get '/manufacturers/:id', to: 'manufacturers#show'
+  get '/boozes', to: 'boozes#index'
+  get '/boozes/:id', to: 'boozes#show'
 end
